d73297c0cff6c8e574c1b971a4867c58fadcb62e
SofijaVvv <sofija.vujosevic@gmail.com>
2025-02-28 14:59:25 +0100

    Removed files, clear localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/layout/navbar/navbar.component';
+import { filter } from 'rxjs';
+import { UserFacade } from './core/auth/services/user.facade';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,21 @@ import { NavbarComponent } from './core/layout/navbar/navbar.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-budget-expense-app';
+  constructor(
+    private router: Router,
+    private userFacade: UserFacade,
+  ) {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationStart => event instanceof NavigationStart,
+        ),
+      )
+      .subscribe((event) => {
+        if (event.navigationTrigger === 'popstate') {
+          this.userFacade.logout();
+          localStorage.clear();
+        }
+      });
+  }
 }
