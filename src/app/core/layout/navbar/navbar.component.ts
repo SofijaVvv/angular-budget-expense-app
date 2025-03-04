f824a7e0cab1fc9fcc521a97056851da8e12f51c
SofijaVvv <sofija.vujosevic@gmail.com>
2025-03-04 13:10:36 +0100

    Sidebar adjustments

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { AsyncPipe, NgClass, NgIf, NgOptimizedImage } from '@angular/common';
 import { UserFacade } from '../../auth/services/user.facade';
 
 @Component({
   selector: 'app-layout-navbar',
   standalone: true,
-  imports: [NgOptimizedImage, AsyncPipe, NgIf],
+  imports: [NgOptimizedImage, AsyncPipe, NgIf, NgClass],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
   isAuthenticated: boolean = false;
+  @Input() isSidebarOpen = false;
 
   constructor(private userFacade: UserFacade) {}
 
@@ -17,5 +18,9 @@ export class NavbarComponent implements OnInit {
     this.userFacade.isAuthenticated$.subscribe((authenticated) => {
       this.isAuthenticated = authenticated;
     });
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
