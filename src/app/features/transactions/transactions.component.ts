f30bff12d8dd67cc70061064a98be92e6458a6cc
SofijaVvv <sofija.vujosevic@gmail.com>
2025-03-04 14:39:15 +0100

    Added new folders/files to features folder

--- src/app/features/transactions/transactions.component.ts
+++ src/app/features/transactions/transactions.component.ts
@@ -5,8 +5,5 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.sass'
 })
-export class TransactionsComponent {
-
-}
+export class TransactionsComponent {}
