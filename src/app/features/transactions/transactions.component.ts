4e14607f49d66c0e5f3fffa789f8c6cc4c687ec3
SofijaVvv <sofija.vujosevic@gmail.com>
2025-03-05 15:39:20 +0100

    feat(table): add reusable table

--- src/app/features/transactions/transactions.component.ts
+++ src/app/features/transactions/transactions.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { TableComponent } from '../../shared/components/table/table.component';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, TableComponent],
   templateUrl: './transactions.component.html',
 })
-export default class TransactionsComponent {}
+export default class TransactionsComponent {
+  transactions = [
+    { id: 1, amount: 100, date: '2025-03-01' },
+    { id: 2, amount: 200, date: '2025-03-02' },
+    { id: 3, amount: 300, date: '2025-03-03' },
+  ];
+
+  displayedColumns = ['id', 'amount', 'date'];
+}
---
+++ src/app/shared/components/table/table.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [NgForOf],
+  templateUrl: './table.component.html',
+})
+export class TableComponent {
+  @Input() tableData: any[] = [];
+  @Input() displayedColumns: string[] = [];
+}
