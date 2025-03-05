0d88b85052c523ff5a10dadd8f71b5ca833010cc
SofijaVvv <sofija.vujosevic@gmail.com>
2025-03-05 15:24:22 +0100

    feat(TransactionService): add CRUD methods

--- src/app/features/transactions/services/transaction.service.ts
+++ src/app/features/transactions/services/transaction.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Transaction } from '../models/transaction.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,26 @@ import { HttpClient } from '@angular/common/http';
 export class TransactionService {
   constructor(private http: HttpClient) {}
 
-  getAll() {}
+  getAll(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>('api/Transactions');
+  }
+
+  getById(id: number): Observable<Transaction> {
+    return this.http.get<Transaction>(`api/Transactions/${id}`);
+  }
+
+  create(transaction: Partial<Transaction>): Observable<Transaction> {
+    return this.http.post<Transaction>('api/Transactions', transaction);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`api/Transactions/${id}`);
+  }
+
+  update(transaction: Partial<Transaction>): Observable<Transaction> {
+    return this.http.put<Transaction>(
+      `api/Transactions/${transaction.id}`,
+      transaction,
+    );
+  }
 }
