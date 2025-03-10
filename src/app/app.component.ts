f20e009b5d171ba11ec1cb212f9b5d210ef0e8c5
SofijaVvv <sofija.vujosevic@gmail.com>
2025-03-10 17:34:36 +0100

    Fixed browser navigation bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NavigationStart, Router, RouterOutlet } from '@angular/router';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationStart } from '@angular/router';
 import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
-import { Subscription } from 'rxjs';
 import { UserFacade } from './core/auth/services/user.facade';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private routerSubscription: Subscription | null = null;
+  private routerSubscription!: Subscription;
   constructor(
     private router: Router,
     private userFacade: UserFacade,
   ) {}
 
   ngOnInit(): void {
-    this.routerSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        if (event.url.includes('/login') || event.url.includes('/register')) {
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((event: any) => {
+        if (event.url === '/login') {
           this.clearUserData();
+          history.pushState(null, '', location.href);
         }
-      }
-    });
+      });
   }
 
   ngOnDestroy(): void {
@@ -34,7 +35,20 @@ export class AppComponent implements OnInit, OnDestroy {
     }
   }
 
-  private clearUserData() {
+  @HostListener('window:beforeunload', ['$event'])
+  handleBeforeUnload(event: Event): void {
+    this.clearUserData();
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: PopStateEvent): void {
+    if (location.pathname === '/login') {
+      this.clearUserData();
+      history.pushState(null, '', location.href);
+    }
+  }
+
+  private clearUserData(): void {
     localStorage.clear();
     this.userFacade.logout();
   }
